ac8785f07f1208d288aeff9670cc8be72cb9fabe
Igor <igorwebber21@gmail.com>
2021-12-07 18:58:28 +0200

    14. Add Ckeditor to Admin

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -303,5 +303,73 @@
   });
 </script>
 
+<script src="{{ asset('assets/admin/ckeditor5/build/ckeditor.js') }}"></script>
+<script src="{{ asset('assets/admin/ckfinder/ckfinder.js') }}"></script>
+
+<script>
+  ClassicEditor
+    .create( document.querySelector( '#content' ), {
+      ckfinder: {
+        uploadUrl: '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files&responseType=json'
+      },
+      image: {
+        // You need to configure the image toolbar, too, so it uses the new style buttons.
+        toolbar: [ 'imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight' ],
+
+        styles: [
+          // This option is equal to a situation where no style is applied.
+          'full',
+
+          // This represents an image aligned to the left.
+          'alignLeft',
+
+          // This represents an image aligned to the right.
+          'alignRight'
+        ]
+      },
+      toolbar: {
+        items: [
+          'heading',
+          '|',
+          'bold',
+          'italic',
+          'link',
+          'bulletedList',
+          'numberedList',
+          '|',
+          'indent',
+          'outdent',
+          'alignment',
+          '|',
+          'blockQuote',
+          'insertTable',
+          'undo',
+          'redo',
+          'CKFinder',
+          'mediaEmbed'
+        ]
+      },
+      language: 'ru',
+      table: {
+        contentToolbar: [
+          'tableColumn',
+          'tableRow',
+          'mergeTableCells'
+        ]
+      },
+    } )
+    .catch( function( error ) {
+      console.error( error );
+    } );
+
+  ClassicEditor
+    .create( document.querySelector( '#description' ), {
+      toolbar: [ 'heading', '|', 'bold', 'italic', '|', 'undo', 'redo' ]
+    } )
+    .catch( function( error ) {
+      console.error( error );
+    } );
+</script>
+
 </body>
 </html>
